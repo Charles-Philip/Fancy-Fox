c47efaf9a29f6c39297f6e4ee8b53299051915c8
Hoai An Nguyen <71863184+anamatxcova@users.noreply.github.com>
2022-11-30 00:58:17 -0800

    Update workout.js

--- workout.js
+++ workout.js
@@ -22,7 +22,7 @@ var target;
 var time;
 
 function setup() {
-    var canvas = createCanvas(640, 480);
+    var canvas = createCanvas(440, 280);
     canvas.position(0, 50);
     video = createCapture(VIDEO);
     video.hide();
