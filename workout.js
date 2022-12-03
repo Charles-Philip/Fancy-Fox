602ebc2cfb24048999ec6533374676aeef46c2f1
Hoai An Nguyen <71863184+anamatxcova@users.noreply.github.com>
2022-12-02 19:12:54 -0800

    Update workout.js

--- workout.js
+++ workout.js
@@ -38,7 +38,7 @@ function setup() {
     canvas = createCanvas(640, 480);
     //centerCanvas();
 
-    canvas.position(0, 110);
+    canvas.position(100, 60);
 //     rectMode(CENTER);
     video = createCapture(VIDEO);
 //     video.size(640, 480);
